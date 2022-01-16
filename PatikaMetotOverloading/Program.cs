c0bf18fc214ec797effcdb51050d7dc89e92dfd9
Büşra Yeşilalioğlu <88002831+7Auri@users.noreply.github.com>
2022-01-16 22:41:57 +0300

    Add files via upload

---
+++ PatikaMetotOverloading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PatikaMetotOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "777";
+
+
+           bool result= int.TryParse(num, out int outNum);
+            if (result)
+            {
+                Console.WriteLine("Successful");
+                Console.WriteLine(outNum);
+            }
+            else
+            {
+
+                Console.WriteLine("Unsuccessful");
+            }
+            Metotlar instance = new Metotlar();
+            instance.Addition(2, 3, out int additionResult);
+            Console.WriteLine(additionResult);
+            //Metot Overloading
+            int number = 8888 ;
+            instance.Print(number);
+            instance.Print("Büşra Yeşilalioğlu");
+        }
+    }
+    class Metotlar
+    {
+        public void  Addition(int a , int b, out int addition)
+        {
+            addition = a + b;
+        }
+        public void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public void Print(int str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public void Print(string str1,string str2)
+        {
+            Console.WriteLine(str1,str2);
+        }
+    }
+}
+
